09810d1256e445bde2bd71a0d769d353a58c0459
Vivek Mourya <rohanmourya879@gmail.com>
2024-03-31 18:56:50 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Gamma from './components/Gamma/Gamma';
+import Flavonoids from './components/flavnoids/Flavonoids';
+import Nav from './components/Nav/Nav';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <BrowserRouter>
+     <Nav/>
+     <Routes>
+        <Route path="/" element={<Gamma/>}/>
+        <Route path="/flav" element={<Flavonoids/>}/>
+
+        <Route/>
+    
+
+     </Routes>
+     
+     </BrowserRouter>
+
+
     </div>
   );
 }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Nav.css'
+
+const Nav = () => {
+  return (
+    
+    <div class="topnav">
+    <a  href="/">Gamma</a>
+    <a href="/flav">Flavonoids</a>
+    
+  </div>
+  )
+}
+
+export default Nav
